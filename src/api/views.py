271c58f521afbd15633a9baca488b99498fc8916
jordanjenner <jordan.jenner12@gmail.com>
2020-03-31 16:24:04 +0100

    Analyse address returns a random number for the time being to test UI

--- src/api/views.py
+++ src/api/views.py
@@ -5,6 +5,8 @@ import random
 # Create your views here.
 
 def analyse_address(request, address, *args, **kwargs):
-    percent = random.randint(0, 100)/100
-    json = {'percentage': percent}
-    return JsonResponse(json)
+    if request.method == "POST":
+        print(address)
+        percent = random.randint(0, 100)/100
+        json = {'percentage': percent}
+        return JsonResponse(json)
