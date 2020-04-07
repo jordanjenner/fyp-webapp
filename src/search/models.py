58dc1c714d056baa0ed55a04865c3afd67a60167
jordanjenner <jordan.jenner12@gmail.com>
2020-04-07 17:31:33 +0100

    Added search app to store data about addresses.

--- src/search/models.py
+++ src/search/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Address(models.Model):
+    address = models.CharField(max_length=35, primary_key=True)
+    last_updated = models.DateTimeField()
+    date_created = models.DateTimeField()
+    update_state = models.CharField(max_length=15)
+
+class Runs(models.Model):
+    address = models.ForeignKey('Address', on_delete=models.CASCADE)
+    percentage = models.IntegerField()
+    transaction_count = models.IntegerField()
+    date_time = models.DateTimeField(auto_now=True)
